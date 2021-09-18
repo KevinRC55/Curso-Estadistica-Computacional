fbe4db7ecec56dbee1178817516508852b214a26
Kevin Rivera <kevinrc.unam@gmail.com>
2021-09-17 21:40:11 -0500

    Actualizacion README.md y Manejo_de_datos.R

--- Manejo_de_datos/Manejo_de_datos.R
+++ Manejo_de_datos/Manejo_de_datos.R
@@ -39,3 +39,99 @@ cualitativas <- select(datos_ordenados, Estado, TipoDelito, SubtipoDelito, Modal
 cuantitativas <- select(datos_ordenados, Año, 7:18)
 
 #------------------------------------------Datos faltantes e imputación--------------------------------------
+
+#Revisamos si tenemos datos faltantes
+
+library(naniar)
+library(VIM)
+
+gg_miss_var(datos_ordenados, show_pct = TRUE)  # muestra % de faltantes
+
+aggr(datos_ordenados,col=c("blue", "red"),
+     combined= F, bars=F, numbers=T,sortVars= T, 
+     prop = c(TRUE, FALSE))# conjunto de renglones con faltantes
+
+
+#Generamos datos faltantes
+library(missMethods)
+
+#Elegimos las columnas donde generaremos faltantes
+columns <- c("Enero", "Julio", "Diciembre")
+
+datos_incompletos <- delete_MCAR(datos_ordenados, 0.15, cols_mis = columns)  # elimina el 15%
+
+#Observamos los datos faltantes generados
+library(UpSetR)
+
+vis_miss(datos_incompletos) # las lineas negras indican la posicion del dato faltante en cada columna
+
+datos_completos <- datos_ordenados
+
+library(ggplot2)
+
+datos_completos$missEnero <- is.na(datos_incompletos$Enero)
+datos_completos$missJulio <- is.na(datos_incompletos$Julio)
+datos_completos$missDiciembre <- is.na(datos_incompletos$Diciembre)
+
+ggplot(datos_completos, aes(x = Enero, y = Año, col = missEnero)) + geom_point()
+ggplot(datos_completos, aes(x = Julio, y = Año, col = missJulio)) + geom_point()
+ggplot(datos_completos, aes(x = Diciembre, y = Año, col = missDiciembre)) + geom_point()
+
+#Comprobamos el patron MCAR
+mcar_test(datos_incompletos)
+# DECISION: valor alto de ji-cuadrada y p-value bajo: los datos no son MCAR
+
+#Imputamos con el metodo mean de mice para datos numericos.
+library(mice)
+
+imputed_data <- mice((datos_incompletos)[,names(datos_incompletos) %in% columns],m = 1, maxit = 1, 
+                     method = "mean")
+
+complete.data <- mice::complete(imputed_data)
+
+#Redondeamos nuestros datos, ya que es la cantidad de registros de incidentes delictivos
+complete.data <- mutate_if(complete.data, is.numeric, round)
+
+datos_imputados <- datos_incompletos
+datos_imputados$Enero <- complete.data$Enero
+datos_imputados$Julio <- complete.data$Julio
+datos_imputados$Diciembre <- complete.data$Diciembre
+
+write.csv(datos_imputados,'Incidencia_Delictiva_Imputados.csv', row.names = FALSE, fileEncoding = "UTF-8")
+
+#Comparamos diferentes metodos de imputacion de mice
+
+#sample
+imputed_data <- mice((datos_incompletos)[,names(datos_incompletos) %in% columns],m = 1, maxit = 1, 
+                     method = "sample")
+
+imputados_sample <- mice::complete(imputed_data)
+imputados_sample <- mutate_if(imputados_sample, is.numeric, round)
+
+#norm.nob
+imputed_data <- mice((datos_incompletos)[,names(datos_incompletos) %in% columns],m = 1, maxit = 1, 
+                     method = "norm.nob")
+
+imputados_norm.nob <- mice::complete(imputed_data)
+imputados_norm.nob <- mutate_if(imputados_norm.nob, is.numeric, round)
+
+#cart
+imputed_data <- mice((datos_incompletos)[,names(datos_incompletos) %in% columns],m = 1, maxit = 1, 
+                     method = "cart")
+
+imputados_cart <- mice::complete(imputed_data)
+imputados_cart <- mutate_if(imputados_cart, is.numeric, round)
+
+#rf
+imputed_data <- mice((datos_incompletos)[,names(datos_incompletos) %in% columns],m = 1, maxit = 1, 
+                     method = "rf")
+
+imputados_rf <- mice::complete(imputed_data)
+imputados_rf <- mutate_if(imputados_rf, is.numeric, round)
+
+#Comparamos con summary
+summary(complete.data) #Datos imputados con mean
+summary(imputados_sample)
+summary(imputados_norm.nob)
+summary(imputados_cart)
+summary(imputados_rf)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Curso de Estadística Computacional
 
 Actividades implementadas en R durante el Curso de Estadística Computacional
+
+Los datos utilizados en este repositorio son recuperados de: https://www.gob.mx/sesnsp/acciones-y-programas/datos-abiertos-de-incidencia-delictiva
