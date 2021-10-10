06a4dd824ed28dd3749e7a53f4f9e1a2b3bf0a0a
Kevin Rivera <kevinrc.unam@gmail.com>
2021-10-09 21:28:03 -0500

    Interpretaciones faltantes

--- Descripcion_Estadistica/Descripcion_Estadistica.R
+++ Descripcion_Estadistica/Descripcion_Estadistica.R
@@ -21,18 +21,18 @@ str(datos)
 ##  SubtipoDelito         #        Cualitativa       # Ordinal   ##
 ##  Modalidad             #        Cualitativa       # Nominal   ##
 ##  BienJuridicoAfectado  #        Cualitativa       # Nominal   ##
-##  Enero                 #  Cualitativa / Discreta  # Razón     ##
-##  Febrero               #  Cualitativa / Discreta  # Razón     ##
-##  Marzo                 #  Cualitativa / Discreta  # Razón     ##
-##  Abril                 #  Cualitativa / Discreta  # Razón     ##
-##  Mayo                  #  Cualitativa / Discreta  # Razón     ##
-##  Junio                 #  Cualitativa / Discreta  # Razón     ##
-##  Julio                 #  Cualitativa / Discreta  # Razón     ##
-##  Agosto                #  Cualitativa / Discreta  # Razón     ##
-##  Mayo            #  Cualitativa / Discreta  # Razón     ##
-##  Octubre               #  Cualitativa / Discreta  # Razón     ##
-##  Noviembre             #  Cualitativa / Discreta  # Razón     ##
-##  Diciembre             #  Cualitativa / Discreta  # Razón     ##
+##  Enero                 #  Cuantitativa / Discreta # Razón     ##
+##  Febrero               #  Cuantitativa / Discreta # Razón     ##
+##  Marzo                 #  Cuantitativa / Discreta # Razón     ##
+##  Abril                 #  Cuantitativa / Discreta # Razón     ##
+##  Mayo                  #  Cuantitativa / Discreta # Razón     ##
+##  Junio                 #  Cuantitativa / Discreta # Razón     ##
+##  Julio                 #  Cuantitativa / Discreta # Razón     ##
+##  Agosto                #  Cuantitativa / Discreta # Razón     ##
+##  Mayo                  #  Cuantitativa / Discreta # Razón     ##
+##  Octubre               #  Cuantitativa / Discreta # Razón     ##
+##  Noviembre             #  Cuantitativa / Discreta # Razón     ##
+##  Diciembre             #  Cuantitativa / Discreta # Razón     ##
 ###################################################################
 
 #========================= Análisis descriptivo de variables cualitativas ====================================
@@ -50,6 +50,13 @@ acumulada_r_delito <- cumsum(relativa_delito)
 (tabla_frec <- cbind(Absoluta = absoluta_delito, A_Acumulada = acumulada_a_delito, 
                      Relativa = relativa_delito, R_Acumulada = acumulada_r_delito))
 
+# De acuerdo a la organización de nuestros datos, en la tabla de frecuencias aun no observamos los delitos
+# con mayor registro de incidencias. Pero podemos observar la cantidad de modalidades en las que generalmente
+# se cometen los diferentes delitos, es decir, la variedad de combinaciones de tipo de delito con subtipo 
+# de delito, modalidad y bien jurídico afectado.
+# En nuestra tabla observamos claramente cinco delitos con mas modalidades, estos son, robo, homicidio,
+# lesiones, secuestro y feminicidio.
+
 # Análisis gráfico
 
 library(ggplot2)
@@ -63,6 +70,15 @@ ggplot(as.data.frame(relativa_delito), aes(x=Var1, y=Freq)) +
   geom_bar(stat="identity", color="blue3", fill="white") + 
   labs(title = "Diagrama de barras - Tipo de delito", subtitle = "Frecuencias Relativas", 
        x = NULL, y = NULL) + coord_flip()
+
+# Con las gráficas anteriores podemos afirmar los que veíamos en nuestra tabla de frecuencias, se observan
+# claramente los delitos con mas modalidades, ademas, podemos apreciar como todos los otros tipos de delitos
+# tienen la misma cantad de modalidades.
+# Es importante aclarar que por la forma en que se presentan nuestros datos, los registros de diferentes
+# modalidades de delitos se repiten para cada estado de la república, contando con 32 estados, las
+# frecuencias deben ser divididas entre 32 para tener las cantidades de modalidades exactas.
+
+length(unique(datos$Estado))
 
 ggplot(as.data.frame(relativa_delito), aes(x=2, y=Freq, fill=names(relativa_delito))) +
   geom_bar(stat="identity", color="white") + coord_polar("y", start=0) +
@@ -88,12 +104,12 @@ range(datos$Mayo)
 
 table(datos$Mayo)
 
-length(datos$Mayo)
-
 #Usando intervalos de clase
-(tabla_mayo <- fdt(datos$Mayo,breaks="Sturges",start=0,end=3750,h=150,right=T))
+(tabla_mayo <- fdt(datos$Mayo,start=0,end=3750,h=150))
 
-#Podemos ver que a partir de registros de delitos mayores a 750 incidencias, las frecuencias se reducen 
+#Podemos ver que el 93 por ciento de nuestros valores se encuentran entre 0 y 150 registros de incidencias
+# delictivas, a partir de registros de delitos mayores a 450 incidencias, las frecuencias se reducen.
+# Es claro que tenemos una distribución unimodal.
 
 # Análisis gráfico
 
@@ -114,9 +130,12 @@ plot_ly(datos, x = ~Mayo, type = 'scatter',
 #Diagrama de caja y brazos  
 plot_ly(datos,x = ~Mayo, type = "box")
 
-# Se observa que la mayoria de delitos se encuentran entre 0 y 20, pareciera que 
-# el resto de los registros de delitos son valores atípicos. 
-#Claramente la distribución de delitos es asimétrica hacia la derecha. 
+# En el diagrama de barras y el diagrama de puntos se observa claramente la distribución de delitos 
+# es asimétrica hacia la derecha. 
+# Con el diagrama de caja y brazos podemos identificar que la mayoría de delitos se encuentran entre 
+# 0 y 20, pareciera que el resto de los registros de delitos son valores atípicos, podemos identificar tres
+# que son mas extremos. 
+
 
 
 #Veamos los delitos que corresponden algunos datos atipicos
@@ -136,37 +155,37 @@ plot_ly(subset(datos, Mayo < 50),x = ~Mayo, type = "box")
 
 # Medidas descriptivas
 
-summary(datos$Mayo) 
+summary(datos$Mayo) #Media y mediana
 
 mfv(datos$Mayo) #Moda
 
-quantile(datos$Mayo, probs = c(0.20,0.40,0.60,0.80)) #Cuantiles
+quantile(datos$Mayo, probs = c(0.05,0.25,0.50,0.75,0.95)) #Cuantiles
 
 IQR(datos$Mayo) #Amplitud intercuartílica
 
-var(datos$Mayo); sd(datos$Mayo) #Varianza
-
-mean(abs(na.omit(datos$Mayo)-mean(na.omit(datos$Mayo))))   # error medio (media)
-
-mean(abs(datos$Mayo-median(datos$Mayo)))  # error medio (mediana)
-
-sd(datos$Mayo)/mean(datos$Mayo) #Coeficiente de variación
+sd(datos$Mayo) #Desviación estándar
 
 library(agricolae)
 
 skewness(datos$Mayo) #Coeficiente de asimetría
 
-kurtosis(datos$Mayo) #Coeficiente de curtosis
+# Con nuestras medidas descriptivas podemos obtener la siguiente información:
+# La media es de 38.86, es decir, en el mes de Mayo de registraron en promedio 39 delitos, tenemos una 
+# moda de 0 que representa la cantidad de filas en donde no se registraron incidencias de un tipo 
+# especifico de delito, con los cuantiles podemos reafirmar lo anterior. El valor de la desviación
+# estándar y el coeficiente de asimetria nos da una idea de como están distribuidos nuestros datos y 
+# podemos reafirmar lo que veíamos en el análisis gráfico.
 
 # Grafica de ojiva
 
-h <- as.data.frame(tabla_mayo$table)
+h <- fdt(datos$Mayo,start=0,end=3700,h=50)
+h <- as.data.frame(h$table)
 
 plot_ly(data=h,x= ~`Class limits` ,y= ~`cf(%)`, 
-        marker = list(size = 10, color="red"),
+        marker = list(size = 5, color="red"),
         type = 'scatter', mode = 'lines')%>%
-  layout(title = 'Ojiva (Dalitos Mayo)', 
-         xaxis = list(title = 'Límite superior'), 
+  layout(title = 'Ojiva (Delitos Mayo)', 
+         xaxis = list(title = 'Intervalos'), 
          yaxis = list(title = 'Pi'),
          shapes = list(
            # linea vertical 
@@ -175,6 +194,12 @@ plot_ly(data=h,x= ~`Class limits` ,y= ~`cf(%)`,
            # linea horizontal 
            list(type = "line", x0 = 0, x1 = 50000, 
                 y0 = 1, y1 = 1, xref = "paper")))
+
+quantile(datos$Mayo, c(.15, .60, .95))
+
+# Debido a la frecuencia en los valores de nuestros datos, en la gráfica de ojiva podemos identificar los
+# porcentiles a patir de 0.85, el valor del porcentil 0.95 esta en el intervalo de 150 a 200, valor que 
+# verificar calculando los porcentiles de manera directa en R.
 
 #=============================comparación y asociación entre variables======================================
 
